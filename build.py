befce186a507638f9945194fac35b4e14f131f32
Mihir Singh <git.service@mihirsingh.com>
2015-03-11 16:45:47 -0500

    Cleaned up and created the structure for a docset

--- build.py
+++ build.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup 
 import requests
+import shutil
+import os
 
 INDEX = 'https://technet.microsoft.com/en-us/library/jj821831%28v=sc.20%29.aspx'
+
+if os.path.exists('cmdlet.docset'):
+    shutil.rmtree('cmdlet.docset')
+os.makedirs('cmdlet.docset/Contents/Resources/Documents/')
 
 page = requests.get(INDEX).content
 soup = BeautifulSoup(page)
