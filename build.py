3efef48794df3c421f119e096a48c6da963aa9bf
Mihir Singh <git.service@mihirsingh.com>
2015-03-12 00:58:18 -0500

    Updated the build script to use the Entry class

--- build.py
+++ build.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup
 import yaml
 import requests
-import shutil
-import os
-import sqlite3
-from docset import DocSet
+from docset import DocSet, Entry
 
 indexes = yaml.load(open('indexes.yaml', 'r').read())['cmdlet']
 
@@ -17,9 +14,6 @@ for index in indexes:
     page = requests.get(index['url']).content
     soup = BeautifulSoup(page)
 
-    if not os.path.exists('PowerShell.docset/Contents/Resources/Documents/'+index['name']):
-        os.mkdir('PowerShell.docset/Contents/Resources/Documents/'+index['name'])
-
     for div in soup.find_all('div'):
 
         try:
@@ -27,88 +21,14 @@ for index in indexes:
                 link = div.a.attrs['href'].strip()
                 title = div.a.attrs['title']
 
-                destination = open('PowerShell.docset/Contents/Resources/Documents/'+index['name']+'/'+title+'.html', 'w')
-                destination.write(requests.get(link).content)
-                destination.close()
-
-                entries.append({
-                                'name': title,
-                                'group': index['name'],
-                                'path': 'PowerShell.docset/Contents/Resources/Documents/'+index['name']+'/'+title+'.html'
-                               })
-
-                print 'Downloaded %s/%s' % (index['name'],title)
+                entries.append(
+                    Entry(title, index['name']+'-'+title+'.html', 'Command', link, docset))
 
         except KeyError:
             pass
 
-print 'Finished downloading'
+for entry in entries: entry.download()
 
 docset.insert_entries(entries)
 
-print 'Inserted documents'
-
-for entry in entries:
-
-    source = open(entry['path'], 'r+')
-
-    soup = BeautifulSoup(source.read())
-
-    try:
-        soup.find(id='megabladeContainer').decompose()
-    except AttributeError:
-        pass
-
-    try:
-        soup.find(id='ux-header').decompose()
-    except AttributeError:
-        pass
-
-    try:
-        soup.find(id='isd_print').decompose()
-    except AttributeError:
-        pass
-
-    try:
-        soup.find(id='isd_printABook').decompose()
-    except AttributeError:
-        pass
-
-    try:
-        soup.find(id='expandCollapseAll').decompose()
-    except AttributeError:
-        pass
-
-    try:
-        soup.find(id='leftNav').decompose()
-    except AttributeError:
-        pass
-
-    try:
-        soup.find_all('div', class_='feedbackContainer')[0].decompose()
-    except AttributeError:
-        pass
-    except IndexError:
-        pass
-
-    try:
-        soup.find_all('div', class_='communityContentContainer')[0].decompose()
-    except AttributeError:
-        pass
-    except IndexError:
-        pass
-
-    try:
-        soup.find(id='ux-footer').decompose()
-    except AttributeError:
-        pass
-
-    for link in soup.find_all('a'):
-        for entry in entries:
-            if entry['name'] == link.get_text():
-                link.attrs['href'] = '../' + entry['group']+'/'+entry['name']+'.html'
-
-    source.seek(0)
-    source.write(str(soup))
-    source.truncate()
-    source.close()
+for entry in entries: entry.rewrite(entries)
