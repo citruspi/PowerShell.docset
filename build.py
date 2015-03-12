712a0162daad84f1de87ef2a97e5c8ac78349c5e
Mihir Singh <git.service@mihirsingh.com>
2015-03-12 01:13:58 -0500

    Merged the docset module and the build script

--- build.py
+++ build.py
@@ -1,7 +1,137 @@
 from bs4 import BeautifulSoup
 import yaml
 import requests
-from docset import DocSet, Entry
+import sqlite3
+
+class DocSet(object):
+
+    def __init__(self, name):
+
+        self.name = name
+        self.init_db()
+        self.entries = []
+
+    @property
+    def path(self):
+
+        return '{name}.docset'.format(name = self.name)
+
+    def init_db(self):
+
+        path = '{path}/Contents/Resources/docSet.dsidx'.format(path = self.path)
+
+        database = sqlite3.connect(path)
+
+        cursor = database.cursor()
+
+        try: cursor.execute('DROP TABLE searchIndex;')
+        except: pass
+
+        cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+        cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+
+        database.commit()
+        database.close()
+
+    def insert_entries(self):
+
+        path = '{path}/Contents/Resources/docSet.dsidx'.format(path = self.path)
+
+        database = sqlite3.connect(path)
+
+        cursor = database.cursor()
+
+        inserts = [(entry.name, entry.type_, entry.path) for entry in self.entries]
+
+        cursor.executemany('insert into searchIndex(name, type, path) values (?,?,?)', inserts)
+
+        database.commit()
+        database.close()
+
+    def create(self):
+
+        for entry in self.entries: entry.download()
+        for entry in self.entries: entry.rewrite(self.entries)
+        self.insert_entries()
+
+class Entry(object):
+
+    def __init__(self, name, path, type_, url, docset):
+
+        self.name = name
+        self.type_ = type_
+        self.url = url
+        self.docset = docset
+        self.path = path
+
+    @property
+    def full_path(self):
+
+        return '{docset}/Contents/Resources/Documents/{path}'.format(
+                                                    docset = self.docset.path,
+                                                    path = self.path)
+
+    def download(self):
+
+        r = requests.get(self.url)
+
+        if r.status_code == 200:
+
+            with open(self.full_path, 'w') as f:
+
+                f.write(r.content)
+
+        else:
+
+            raise Exception('Received "{code}" when downloading "{name}"'.format(
+                                                        code = r.status_code,
+                                                        name = self.name))
+
+    def rewrite(self, entries):
+
+        source = open(self.full_path, 'r+')
+
+        soup = BeautifulSoup(source.read())
+
+        unnecessary = [
+            '#megabladeContainer',
+            '#ux-header',
+            '#isd_print',
+            '#isd_printABook',
+            '#expandCollapseAll',
+            '#leftNav',
+            '.feedbackContainer',
+            '#isd_printABook',
+            '.communityContentContainer',
+            '#ux-footer'
+        ]
+
+        for u in unnecessary:
+
+            if u[0] == '#':
+
+                try:
+                    soup.find(id=u[1:]).decompose()
+                except AttributeError:
+                    pass
+
+            elif u[0] == '.':
+
+                for element in soup.find_all('div', class_=u[1:]):
+                    element.decompose()
+
+        for link in soup.find_all('a'):
+            for entry in entries:
+                try:
+                    if link.attrs['href'] == entry.url:
+                        link.attrs['href'] = entry.path
+                except KeyError:
+                    pass
+
+        source.seek(0)
+        source.write(str(soup))
+        source.truncate()
+        source.close()
 
 if __name__ == '__main__':
 
@@ -24,7 +154,7 @@ if __name__ == '__main__':
                     title = div.a.attrs['title']
 
                     docset.entries.append(
-                        Entry(title, group+'-'+title+'.html', 'Command', link))
+                        Entry(title, group+'-'+title+'.html', 'Command', link, docset))
 
             except KeyError:
                 pass
